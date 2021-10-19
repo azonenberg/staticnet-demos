b5b8c9f139d364b5ec74a4bd3a99f7efdf6e0962
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2021-10-19 01:32:58 -0700

    Continued initial bringup of STM32 Ethernet

--- stm32/sshcli/main.cpp
+++ stm32/sshcli/main.cpp
@@ -31,15 +31,117 @@
 #include <peripheral/Flash.h>
 #include <peripheral/GPIO.h>
 #include <peripheral/RCC.h>
+#include <peripheral/Timer.h>
+#include <util/Logger.h>
 #include <cli/UARTOutputStream.h>
 #include "DemoCLISessionContext.h"
 
 //UART console
 UART* g_cliUART = NULL;
+Logger g_log;
 UARTOutputStream g_uartStream;
 DemoCLISessionContext g_uartCliContext;
 
+//need a total of 4 descriptors in the ring
+static volatile uint8_t buffers[4][2048];
+static volatile edma_rx_descriptor_t rx_dma_descriptor[4];
+
+void InitClocks();
+void InitLog();
+void InitUART();
+void InitEthernet();
+
 int main()
+{
+	//Hardware setup
+	InitClocks();
+	InitUART();
+	InitLog();
+	InitEthernet();
+
+	//Initialize the CLI on the console UART interface
+	g_uartStream.Initialize(g_cliUART);
+	g_uartCliContext.Initialize(&g_uartStream, "admin");
+
+	//Enable interrupts only after all setup work is done
+	EnableInterrupts();
+
+	//Show the initial prompt
+	//g_uartCliContext.PrintPrompt();
+
+	//TODO: use loopback mode to sanity check
+
+	//Main event loop
+	int nextRxFrame = 0;
+	uint32_t numRxFrames = 0;
+	uint32_t numRxBad = 0;
+	while(1)
+	{
+		//Check if we have a new frame ready to process
+		auto& desc = rx_dma_descriptor[nextRxFrame];
+		if( (desc.RDES0 & 0x80000000) == 0)
+		{
+			numRxFrames ++;
+
+			int len = (desc.RDES0 >> 16) & 0x3fff;
+			g_log("Got a frame (%d bytes) at descriptor %d\n", len, nextRxFrame);
+			LogIndenter li(g_log);
+
+			g_log("RDES = %08x %08x %08x %08x \n", desc.RDES0, desc.RDES1, desc.RDES2, desc.RDES3);
+			if(desc.RDES0 & 0x2)
+			{
+				g_log(Logger::ERROR, "CRC error\n");
+				numRxBad ++;
+			}
+
+			g_log("CRC / total: %d / %d", numRxBad, numRxFrames);
+			for(int i=0; i<len; i++)
+			{
+				if( (i & 63) == 0)
+					g_cliUART->Printf("\n    ");
+				g_cliUART->Printf("%02x ", buffers[nextRxFrame][i]);
+			}
+			g_cliUART->Printf("\n");
+
+			//Done processing the frame, return it to the MAC
+			desc.RDES0 |= 0x80000000;
+			EDMA.DMARPDR = 0;
+
+			if(nextRxFrame == 3)
+				nextRxFrame = 0;
+			else
+				nextRxFrame ++;
+
+			//Reset perf counters so they don't wrap
+			if(numRxFrames >= 0x80000000)
+			{
+				numRxFrames = 0;
+				numRxBad = 0;
+			}
+
+			//If we get a CRC error in the first 100 frames, reset the MAC
+			if( (numRxFrames < 100) && numRxBad)
+			{
+				g_log(Logger::ERROR, "CRC error seen on early packet, resetting MAC...\n");
+				numRxFrames = 0;
+				numRxBad = 0;
+				InitEthernet();
+			}
+		}
+
+		/*
+		//Wait for an interrupt
+		asm("wfi");
+
+		if(g_cliUART->HasInput())
+			g_uartCliContext.OnKeystroke(g_cliUART->BlockingRead());
+		*/
+	}
+
+	return 0;
+}
+
+void InitClocks()
 {
 	//Configure the flash with wait states and prefetching before making any changes to the clock setup.
 	//A bit of extra latency is fine, the CPU being faster than flash is not.
@@ -57,34 +159,97 @@ int main()
 		2);		//Divide AHB clock by 2 to get APB2 clock (87.5 MHz)
 
 	//TODO: Debug clock output on MCO1 (PA8) pmod, see 5.2.10?
+}
 
+void InitLog()
+{
+	//APB1 is 43.75 MHz, divide down to get 1 kHz ticks
+	static Timer logtim(&TIM2, Timer::FEATURE_GENERAL_PURPOSE, 43750);
+
+	g_log.Initialize(g_cliUART, &logtim);
+	g_log("UART logging ready\n");
+}
+
+void InitUART()
+{
 	//Initialize the UART for local console: 115.2 Kbps using PA0 for UART4 transmit and PA3 for USART2 RX
 	//TODO: nice interface for enabling UART interrupts
 	GPIOPin uart_tx(&GPIOA, 0, GPIOPin::MODE_PERIPHERAL, GPIOPin::SLEW_SLOW, 8);
 	GPIOPin uart_rx(&GPIOA, 3, GPIOPin::MODE_PERIPHERAL, GPIOPin::SLEW_SLOW, 7);
-	UART uart(&UART4, &USART2, 380);
+	static UART uart(&UART4, &USART2, 380);
 	volatile uint32_t* NVIC_ISER1 = (volatile uint32_t*)(0xe000e104);
 	*NVIC_ISER1 = 0x40;
 	g_cliUART = &uart;
+}
 
-	//Initialize the CLI on the console UART interface
-	g_uartStream.Initialize(g_cliUART);
-	g_uartCliContext.Initialize(&g_uartStream, "admin");
+void InitEthernet()
+{
+	g_log("Initializing Ethernet\n");
 
-	//Enable interrupts only after all setup work is done
-	EnableInterrupts();
+	//Initialize the Ethernet pins. AF11 on all pins
+	GPIOPin rmii_refclk(&GPIOA, 1, GPIOPin::MODE_PERIPHERAL, 11);
+	GPIOPin rmii_mdio(&GPIOA, 2, GPIOPin::MODE_PERIPHERAL, 11);
+	GPIOPin rmii_crs_dv(&GPIOA, 7, GPIOPin::MODE_PERIPHERAL, 11);
+	GPIOPin rmii_tx_en(&GPIOB, 11, GPIOPin::MODE_PERIPHERAL, GPIOPin::SLEW_FAST, 11);
+	GPIOPin rmii_txd0(&GPIOB, 12, GPIOPin::MODE_PERIPHERAL, GPIOPin::SLEW_FAST, 11);
+	GPIOPin rmii_txd1(&GPIOB, 13, GPIOPin::MODE_PERIPHERAL, GPIOPin::SLEW_FAST, 11);
+	GPIOPin rmii_mdc(&GPIOC, 1, GPIOPin::MODE_PERIPHERAL, 11);
+	GPIOPin rmii_rxd0(&GPIOC, 4, GPIOPin::MODE_PERIPHERAL, 11);
+	GPIOPin rmii_rxd1(&GPIOC, 5, GPIOPin::MODE_PERIPHERAL, 11);
 
-	//Show the initial prompt
-	g_uartCliContext.PrintPrompt();
+	//Ignore the MDIO bus for now
 
-	while(1)
+	//Select RMII mode
+	//TODO: put this in RCCHelper
+	//Disable all Ethernet clocks (except 1588 which we don't need, leave it off to save power), reset MAC
+	RCC.AHB1ENR &= ~(RCC_AHB1_EMAC | RCC_AHB1_EMAC_TX | RCC_AHB1_EMAC_RX);
+	RCC.AHB1RSTR |= RCC_AHB1_EMAC;
+
+	//Enable SYSCFG before changing any settings on it
+	RCC.APB2ENR |= RCC_APB2_SYSCFG;
+
+	//Enable RMII
+	SYSCFG.PMC |= ETH_MODE_RMII;
+
+	//Clear resets
+	RCC.AHB1RSTR &= ~RCC_AHB1_EMAC;
+
+	//Enable Ethernet clocks (except 1588 since we don't use that)
+	RCC.AHB1ENR |= RCC_AHB1_EMAC | RCC_AHB1_EMAC_TX | RCC_AHB1_EMAC_RX | RCC_AHB1_PTP;
+
+	//Wait for DMA to finish power-on reset
+	while((EDMA.DMABMR & 1) == 1)
+	{}
+
+	//Receive all frames. promiscuous mode
+	EMAC.MACFFR = 0x80000001;
+
+	/*
+		Create a simple DMA ring with a single descriptor in it
+		see 42.6.8
+		42.8
+	 */
+	for(int i=0; i<4; i++)
 	{
-		//Wait for an interrupt
-		asm("wfi");
+		rx_dma_descriptor[i].RDES0 = 0x80000000;
+		if(i == 3)
+			rx_dma_descriptor[i].RDES1 = 0x00008800;
+		else
+			rx_dma_descriptor[i].RDES1 = 0x00000800;
 
-		if(g_cliUART->HasInput())
-			g_uartCliContext.OnKeystroke(g_cliUART->BlockingRead());
+		rx_dma_descriptor[i].RDES2 = (uint32_t)&buffers[i];
+		rx_dma_descriptor[i].RDES3 = 0;
 	}
 
-	return 0;
+	//Configure DMA descriptor list
+	EDMA.DMARDLAR = &rx_dma_descriptor[0];
+
+	//Select mode: 100/full, RX enabled, no TX, no carrier sense
+	EMAC.MACCR = 0x1c804;
+
+	//Poll demand DMA RX
+	//EDMA.DMARPDR = 0;
+
+	//Enable actual DMA in DMAOMR bits 1/13
+	EDMA.DMAOMR |= 2;
 }
